02de49fada5f82625e631d8005e5ddc6cef545ce
Carsten Thielecke <carsten.thielecke@ieee.org>
2014-11-09 17:26:30 +0100

    Initial commit to tdp.

---
+++ test/helper.rb
@@ -0,0 +1,27 @@
+require 'simplecov'
+
+module SimpleCov::Configuration
+  def clean_filters
+    @filters = []
+  end
+end
+
+SimpleCov.configure do
+  clean_filters
+  load_adapter 'test_frameworks'
+end
+
+ENV["COVERAGE"] && SimpleCov.start do
+  add_filter "/.rvm/"
+end
+require 'rubygems'
+require 'minitest/unit'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'tdp'
+
+class MiniTest::Unit::TestCase
+end
+
+MiniTest::Unit.autorun
